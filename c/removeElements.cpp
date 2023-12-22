3e8277b207a0f72cd9964437b245794e6d050941
Rama Rahardi <rama.orion@gmail.com>
2023-12-22 09:54:57 +0700

    Remove Linked List Elements

---
+++ c/removeElements.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+
+#include <iostream>
+
+#include "ListNode.hpp"
+using namespace std;
+
+class Solution {
+   public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        if (head->val == val) {
+            return removeElements(head->next, val);
+        }
+
+        if (head->next == nullptr) {
+            return head;
+        }
+
+        if (head->next->val == val) {
+            head->next = head->next->next;
+        }
+
+        head->next = removeElements(head->next, val);
+        return head;
+    }
+};
+
+int main() {
+    Solution solution;
+    ListNode* head = new ListNode(
+        1,
+        new ListNode(
+            2,
+            new ListNode(
+                6, new ListNode(
+                       3, new ListNode(4, new ListNode(5, new ListNode(6)))))));
+    printList(head);
+    ListNode* result = solution.removeElements(head, 6);
+    printList(result);
+    return 0;
+}
