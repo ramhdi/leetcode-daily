0cf98e916315fec79a7e13a566d3652bb0ce3548
Rama Rahardi <rama.orion@gmail.com>
2023-12-24 20:47:50 +0700

    Path Crossing

---
+++ rust/src/bin/is_path_crossing.rs
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/path-crossing/
+// 2023/12/24
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn is_path_crossing(path: String) -> bool {
+        let mut point: (i32, i32) = (0, 0);
+        let mut visited_points: HashSet<(i32, i32)> = HashSet::new();
+        visited_points.insert(point);
+
+        for p in path.chars() {
+            match p {
+                'N' => {
+                    point.1 += 1;
+                }
+                'E' => {
+                    point.0 += 1;
+                }
+                'S' => {
+                    point.1 -= 1;
+                }
+                'W' => {
+                    point.0 -= 1;
+                }
+                _ => (),
+            }
+
+            if visited_points.contains(&point) {
+                return true;
+            }
+            visited_points.insert(point);
+        }
+        return false;
+    }
+}
+
+pub struct Solution {}
+
+fn main() {
+    println!("{}", Solution::is_path_crossing("NES".to_string()));
+    println!("{}", Solution::is_path_crossing("NESWW".to_string()));
+}
