b0e4d482669ba27fac4a964e0104c7695697bcbf
Rama Rahardi <rama.orion@gmail.com>
2024-02-26 19:02:37 +0700

    Same tree

---
+++ rust/src/bin/is_same_tree.rs
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/same-tree/
+// 2024/02/26
+
+mod tree_node;
+use crate::tree_node::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (None, Some(_)) | (Some(_), None) => false,
+            (Some(p), Some(q)) => {
+                let p = p.borrow();
+                let q = q.borrow();
+
+                p.val == q.val
+                    && Solution::is_same_tree(p.left.clone(), q.left.clone())
+                    && Solution::is_same_tree(p.right.clone(), q.right.clone())
+            }
+        }
+    }
+}
+
+pub struct Solution {}
+
+fn main() {
+    println!(
+        "{:?}",
+        Solution::is_same_tree(
+            TreeNode::from_array(&[Some(1), Some(2), Some(3)], 0),
+            TreeNode::from_array(&[Some(1), Some(2), Some(3)], 0)
+        )
+    ); // true
+
+    println!(
+        "{:?}",
+        Solution::is_same_tree(
+            TreeNode::from_array(&[Some(1), Some(2)], 0),
+            TreeNode::from_array(&[Some(1), None, Some(2)], 0)
+        )
+    ); // false
+
+    println!(
+        "{:?}",
+        Solution::is_same_tree(
+            TreeNode::from_array(&[Some(1), Some(2), Some(1)], 0),
+            TreeNode::from_array(&[Some(1), Some(1), Some(2)], 0)
+        )
+    ); // false
+}
